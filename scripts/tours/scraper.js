f4cf796fa35e52325ecdee982f5effdd255718c6
Krishna Wijaya <krishnawijaya79@gmail.com>
2025-01-24 14:58:32 +0800

    Updates

--- scripts/tours/scraper.js
+++ scripts/tours/scraper.js
@@ -38,76 +38,83 @@ async function scrape(tour, categoryName) {
             }
         }
 
-        const wells = Array.from(document.querySelectorAll('div.row.row-tour > div.col-lg-9.pad-tour > div.well'))
-        const placeLinks = Array.from(wells[1].querySelectorAll('ul li')).map(link => link?.innerText ?? null).filter(item => item)
+        try {
+            const wells = Array.from(document.querySelectorAll('div.row.row-tour > div.col-lg-9.pad-tour > div.well'))
+            const placeLinks = Array.from(wells[1].querySelectorAll('ul li')).map(link => link?.innerText ?? null).filter(item => item)
 
-        const tourDetails = wells[0].querySelectorAll('h1, h2, h3, p')
-        const title = getProperty(tourDetails[0].querySelector('a'))
-        const tagline = getProperty(tourDetails[1].querySelector('a'))
-        const description = getProperty(tourDetails[2])
+            const tourDetails = wells[0].querySelectorAll('h1, h2, h3, p')
+            const title = getProperty(tourDetails[0].querySelector('a'))
+            const tagline = getProperty(tourDetails[1].querySelector('a'))
+            const description = getProperty(tourDetails[2])
 
-        let tourDetailsPrice = ""
-        let tourDetailsItinerary = ""
-        let tourDetailsInclusion = []
+            let tourDetailsPrice = ""
+            let tourDetailsItinerary = ""
+            let tourDetailsInclusion = []
 
-        wells.splice(0, 2)
-        const placeDetails = wells.map(well => {
-            const contents = well.querySelectorAll('span > div > div')
-            const detailsElement = well.querySelector(':scope > div.row > div')
+            wells.splice(0, 2)
+            const placeDetails = wells.map(well => {
+                const contents = well.querySelectorAll('span > div > div')
+                const detailsElement = well.querySelector(':scope > div.row > div')
 
-            if (contents.length >= 2) {
-                return {
-                    link: getProperty(contents[0].querySelector('a')),
-                    title: getProperty(contents[1].querySelector('h3 a')),
-                    image: getProperty(contents[0].querySelector('a img')),
-                    description: getProperty(contents[1].querySelector('p')),
-                    button: getProperty(contents[1].querySelector('div a')),
-                }
-            }
-
-            else if (detailsElement) {
-                let inclusion = Array.from(detailsElement.children)
-                inclusion.splice(0, 2)
-
-                const tourPrice = detailsElement.querySelector('span#tour-price') ?? detailsElement.querySelector('span#price')
-                const tourItinerary = detailsElement.querySelector('span#tour-itinerary') ?? detailsElement.querySelector('span#itinerary')
-
-                tourDetailsPrice = {
-                    title: tourPrice.querySelector('h3')?.outerHTML,
-                    content: tourPrice?.outerHTML,
+                if (contents.length >= 2) {
+                    return {
+                        link: getProperty(contents[0].querySelector('a')),
+                        title: getProperty(contents[1].querySelector('h3 a')),
+                        image: getProperty(contents[0].querySelector('a img')),
+                        description: getProperty(contents[1].querySelector('p')),
+                        button: getProperty(contents[1].querySelector('div a')),
+                    }
                 }
 
-                tourDetailsItinerary = {
-                    title: tourItinerary.querySelector('h3')?.outerHTML,
-                    content: tourItinerary.querySelector('p')?.outerHTML,
+                else if (detailsElement) {
+                    let inclusion = Array.from(detailsElement.children)
+                    inclusion.splice(0, 2)
+
+                    const tourPrice = detailsElement.querySelector('span#tour-price') ?? detailsElement.querySelector('span#price')
+                    const tourItinerary = detailsElement.querySelector('span#tour-itinerary') ?? detailsElement.querySelector('span#itinerary')
+
+                    tourDetailsPrice = {
+                        title: tourPrice.querySelector('h3')?.outerHTML,
+                        content: tourPrice?.outerHTML,
+                    }
+
+                    tourDetailsItinerary = {
+                        title: tourItinerary.querySelector('h3')?.outerHTML,
+                        content: tourItinerary.querySelector('p')?.outerHTML,
+                    }
+
+                    tourDetailsInclusion = inclusion.map(item => item?.outerHTML ?? null).filter(item => item)
+                    return null
                 }
 
-                tourDetailsInclusion = inclusion.map(item => item?.outerHTML ?? null).filter(item => item)
                 return null
+            }).filter(item => item)
+
+            return {
+                title,
+                tagline,
+                description,
+                placeLinks,
+                placeDetails,
+                tourDetails: {
+                    price: tourDetailsPrice,
+                    itinerary: tourDetailsItinerary,
+                    inclusion: tourDetailsInclusion,
+                },
             }
-
+        } catch (error) {
+            console.error('Error during scraping:', error)
             return null
-        }).filter(item => item)
-
-        return {
-            title,
-            tagline,
-            description,
-            placeLinks,
-            placeDetails,
-            tourDetails: {
-                price: tourDetailsPrice,
-                itinerary: tourDetailsItinerary,
-                inclusion: tourDetailsInclusion,
-            },
         }
     })
+
+    if (!data) return
 
     const dataPath = path.join('data/tours', categoryName, `${tour.title.toLowerCase().replace(/[\s]/g, '_').replace(/[|]/g, '-')}.json`)
     writeFileSync(dataPath, JSON.stringify(data, null, 2))
 
     await browser.close()
-    console.log(`Finished scraping ${tour.title}`)
+    console.info(`Finished scraping ${tour.title}`)
 }
 
 (async () => {
@@ -116,12 +123,15 @@ async function scrape(tour, categoryName) {
             console.log(`Scraping ${category.categoryName}...`)
 
             for (const tour of category.tours) {
-                const isExist = fs.existsSync(`${tour.title.toLowerCase().replace(/[\s]/g, '_').replace(/[|]/g, '-')}.json`)
-                if (!isExist) await scrape(tour, category.categoryName)
+                const dataPath = path.join('data/tours', category.categoryName, `${tour.title.toLowerCase().replace(/[\s]/g, '_').replace(/[|]/g, '-')}.json`)
+                const isExist = fs.existsSync(dataPath)
+
+                if (isExist) console.info('Data already scraped.')
+                else await scrape(tour, category.categoryName)
             }
         }
 
-        console.log('All data scraped successfully!')
+        console.info('All data scraped successfully!')
     } catch (error) {
         console.error('Error during scraping:', error)
     }
